68ac8584ce00c0f11193df99ce24a769dde24c99
Jeff Keller <jrkeller27@gmail.com>
2020-12-09 22:07:48 -0500

    maybe fix timezone display for both utc and american time zones

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,10 +1,15 @@
-import {format, parseISO} from 'date-fns';
-
+// import { parseFromTimeZone, formatToTimeZone } from 'date-fns-tz';
+import { zonedTimeToUtc, utcToZonedTime, format } from 'date-fns-tz';
+// import {format, parseISO} from 'date-fns';
+// const TIMEZONE_OPTS = { timeZone: 'America/New_York' };
+const TIMEZONE = 'America/New_York';
 export function formatMonthSummaryDisplay(dateString: string): string {
-    const date = parseISO(dateString);
-    return format(date, 'MMMM uuuu');
+    const date = zonedTimeToUtc(dateString, TIMEZONE); //parseISO(dateString);
+    return format(utcToZonedTime(date, TIMEZONE), 'MMMM uuuu', { timeZone: TIMEZONE });
 }
 export function formatMicrodataDate(dateString: string): string {
-    const date = parseISO(dateString);
-    return format(date, 'MMMM d, uuuu')
+    const date = zonedTimeToUtc(dateString, TIMEZONE); //parseISO(dateString);
+    return format(utcToZonedTime(date, TIMEZONE), 'MMMM d, uuuu', { timeZone: TIMEZONE });
+    // const date = new Date(dateString); // parseISO(dateString);
+    // return formatToTimeZone(date, 'MMMM d, uuuu', TIMEZONE_OPTS)
 }
